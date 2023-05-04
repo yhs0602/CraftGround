469a063a9bd1a4bbb4a4821bda1ae006b0b24f95
KYHSGeekCode <jourhyang123@gmail.com>
2023-05-04 22:44:23 +0900

    :sparkles: EscapeWardenWrapper

--- wrappers/EscapeWardenWrapper.py
+++ wrappers/EscapeWardenWrapper.py
@@ -1,4 +1,3 @@
-from collections import deque
 from typing import SupportsFloat, Any
 
 import gymnasium as gym
@@ -13,17 +12,17 @@ from wrapper_runner import WrapperRunner
 class EscapeHuskWrapper(gym.Wrapper):
     def __init__(self):
         self.env = mydojo.make(
-            initialInventoryCommands=["minecraft:diamond_sword", "minecraft:shield"],
+            initialInventoryCommands=[],
             initialPosition=None,  # nullable
             initialMobsCommands=[
                 # "minecraft:sheep",
-                "minecraft:husk ~ ~ ~5 {HandItems:[{Count:1,id:iron_shovel},{}]}",
+                "minecraft:warden ~ ~ ~15",
                 # player looks at south (positive Z) when spawn
             ],
-            imageSizeX=114,
-            imageSizeY=64,
-            visibleSizeX=114,
-            visibleSizeY=64,
+            imageSizeX=228,
+            imageSizeY=128,
+            visibleSizeX=228,
+            visibleSizeY=128,
             seed=12345,  # nullable
             allowMobSpawn=False,
             alwaysDay=True,
@@ -41,46 +40,18 @@ class EscapeHuskWrapper(gym.Wrapper):
             shape=(3, initial_env.imageSizeX, initial_env.imageSizeY),
             dtype=np.uint8,
         )
-        self.health_deque = deque(maxlen=2)
-        self.distance_deque = deque(maxlen=2)
 
     def step(
         self, action: WrapperActType
     ) -> tuple[WrapperObsType, SupportsFloat, bool, bool, dict[str, Any]]:
         action_arr = int_to_action(action)
+        action_arr[2] = 2  # must crawl
         obs, reward, terminated, truncated, info = self.env.step(action_arr)
         rgb = obs["rgb"]
         obs = obs["obs"]
         is_dead = obs.is_dead
-        visible_entities = obs.visible_entities
-        if len(visible_entities) > 0:
-            zombie_entity = visible_entities[0]
-            distance = (
-                (obs.x - zombie_entity.x) ** 2
-                + (obs.y - zombie_entity.y) ** 2
-                + (obs.z - zombie_entity.z) ** 2
-            )
-            self.distance_deque.append(distance)
-        else:
-            self.distance_deque.append(self.distance_deque[0])
-        # else:
-        #     print("No visible entities")
-
-        self.health_deque.append(obs.health)
-
-        is_hit = self.health_deque[0] > self.health_deque[1]
-        is_further = self.distance_deque[0] < self.distance_deque[1]
-        is_same = self.distance_deque[0] == self.distance_deque[1]
 
         reward = 1  # initial reward
-        if is_further:
-            reward = 10  # reward for moving away
-        elif is_same:
-            reward = 1
-        else:
-            reward = -10  # penalty for moving closer
-        if is_hit:
-            reward = -20  # penalty
         if is_dead:  #
             if self.initial_env.isHardCore:
                 reward = -10000000
@@ -96,9 +67,6 @@ class EscapeHuskWrapper(gym.Wrapper):
 
     def reset(self, fast_reset: bool = True) -> WrapperObsType:
         obs = self.env.reset(fast_reset=fast_reset)
-        self.health_deque.clear()
-        self.health_deque.append(20)
-        self.distance_deque.append(25)  # start distance: 5
         return obs
 
 
@@ -111,7 +79,7 @@ def main():
     update_freq = 25
     runner = WrapperRunner(
         env,
-        "EscapeHuskWrapper-6Actions-distance",
+        "EscapeWarden15-6Actions",
         buffer_size,
         batch_size,
         gamma,
