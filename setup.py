dbd25f6fb3920f9d22740edf197c84dafab72712
yhs0602 <jourhyang123@gmail.com>
2023-10-04 17:47:27 +0900

    :sparkles: Add jvm dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="craftground",
-    version="0.6",
+    version="0.7",
     packages=find_packages(),
     install_requires=["gymnasium", "Pillow", "numpy", "protobuf", "typing_extensions"],
     author="yhs0602",
