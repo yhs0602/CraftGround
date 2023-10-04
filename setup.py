29241ad046e81e7c81e7f6074fa27b297f4d11f5
yhs0602 <jourhyang123@gmail.com>
2023-10-04 17:52:34 +0900

    :heavy_plus_sign: Add jvem dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="craftground",
-    version="0.8",
+    version="0.9",
     packages=find_packages(),
     install_requires=["gymnasium", "Pillow", "numpy", "protobuf", "typing_extensions"],
     author="yhs0602",
