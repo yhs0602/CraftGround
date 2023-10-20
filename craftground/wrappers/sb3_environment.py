444cc82a21eeb52e612f29a2d93fd5d42cb7031c
yhs0602 <jourhyang123@gmail.com>
2023-10-20 22:15:30 +0900

    :sparkles: Support vecenv of sb3

--- craftground/wrappers/sb3_environment.py
+++ craftground/wrappers/sb3_environment.py
@@ -1,114 +1,135 @@
-from typing import SupportsFloat, Any, Optional, List
+from collections import OrderedDict
+from copy import deepcopy
+from typing import Any, Optional, List, Type, Dict
 
 import gym
-import math
 import numpy as np
-from gymnasium.core import WrapperActType, WrapperObsType
-from gymnasium.vector.utils import spaces
+from stable_baselines3.common.vec_env import VecEnv
+from stable_baselines3.common.vec_env.base_vec_env import (
+    VecEnvIndices,
+    VecEnvStepReturn,
+    VecEnvObs,
+)
+from stable_baselines3.common.vec_env.patch_gym import _patch_env
+from stable_baselines3.common.vec_env.util import (
+    obs_space_info,
+    dict_to_obs,
+    copy_obs_dict,
+)
 
 
 # Sound wrapper
-class StableBaseline3Wrapper(gym.Wrapper):
-    def __init__(
-        self,
-        env,
-        x_dim,
-        y_dim,
-        sound_list: List[str],
-        sound_coord_dim: int = 2,
-        **kwargs,
-    ):
-        self.env = env
-        self.sound_list = sound_list
-        self.sound_coord_dim = sound_coord_dim
-        super().__init__(self.env)
-        self.observation_space = spaces.Dict(
-            {
-                "vision": gym.spaces.Box(
-                    low=0,
-                    high=255,
-                    shape=(3, x_dim, y_dim),
-                    dtype=np.uint8,
-                ),
-                "sound": gym.spaces.Box(
-                    low=-1,
-                    high=1,
-                    shape=(len(sound_list) * sound_coord_dim + 3,),
-                    dtype=np.float32,
-                ),
-            }
+class StableBaseline3Wrapper(VecEnv):
+    actions: np.ndarray
+
+    def step_async(self, actions: np.ndarray) -> None:
+        self.actions = actions
+
+    def step_wait(self) -> VecEnvStepReturn:
+        # Avoid circular imports
+        env_idx = 0
+        obs, self.buf_rews[0], terminated, truncated, self.buf_infos[0] = self.env.step(
+            self.actions[0]
+        )
+        # convert to SB3 VecEnv api
+        self.buf_dones[0] = terminated or truncated
+        # See https://github.com/openai/gym/issues/3102
+        # Gym 0.26 introduces a breaking change
+        self.buf_infos[0]["TimeLimit.truncated"] = truncated and not terminated
+
+        if self.buf_dones[0]:
+            # save final observation where user can get it, then reset
+            self.buf_infos[0]["terminal_observation"] = obs
+            obs, self.reset_infos[0] = self.env.reset()
+        self._save_obs(0, obs)
+        return (
+            self._obs_from_buf(),
+            np.copy(self.buf_rews),
+            np.copy(self.buf_dones),
+            deepcopy(self.buf_infos),
         )
 
-    def step(
-        self, action: WrapperActType
-    ) -> tuple[WrapperObsType, SupportsFloat, bool, bool, dict[str, Any]]:
-        obs, reward, terminated, truncated, info = self.env.step(action)
-        rgb = info["rgb"]
-        obs_info = info["obs"]
-        sound_subtitles = obs_info.sound_subtitles
-        sound_vector = self.encode_sound(
-            sound_subtitles, obs_info.x, obs_info.y, obs_info.z, obs_info.yaw
+    def close(self) -> None:
+        self.env.close()
+
+    def get_attr(self, attr_name: str, indices: VecEnvIndices = None) -> List[Any]:
+        """Return attribute from vectorized environment (see base class)."""
+        target_envs = self._get_target_envs(indices)
+        return [getattr(env_i, attr_name) for env_i in target_envs]
+
+    def set_attr(
+        self, attr_name: str, value: Any, indices: VecEnvIndices = None
+    ) -> None:
+        """Set attribute inside vectorized environments (see base class)."""
+        target_envs = self._get_target_envs(indices)
+        for env_i in target_envs:
+            setattr(env_i, attr_name, value)
+
+    def env_method(
+        self,
+        method_name: str,
+        *method_args,
+        indices: VecEnvIndices = None,
+        **method_kwargs,
+    ) -> List[Any]:
+        """Call instance methods of vectorized environments."""
+        target_envs = self._get_target_envs(indices)
+        return [
+            getattr(env_i, method_name)(*method_args, **method_kwargs)
+            for env_i in target_envs
+        ]
+
+    def env_is_wrapped(
+        self, wrapper_class: Type[gym.Wrapper], indices: VecEnvIndices = None
+    ) -> List[bool]:
+        """Check if worker environments are wrapped with a given wrapper"""
+        target_envs = self._get_target_envs(indices)
+        # Import here to avoid a circular import
+        from stable_baselines3.common import env_util
+
+        return [env_util.is_wrapped(env_i, wrapper_class) for env_i in target_envs]
+
+    def _get_target_envs(self, indices: VecEnvIndices) -> List[gym.Env]:
+        return [self.env]
+
+    def _save_obs(self, env_idx: int, obs: VecEnvObs) -> None:
+        for key in self.keys:
+            if key is None:
+                self.buf_obs[key][0] = obs
+            else:
+                self.buf_obs[key][0] = obs[key]  # type: ignore[call-overload]
+
+    def _obs_from_buf(self) -> VecEnvObs:
+        return dict_to_obs(self.observation_space, copy_obs_dict(self.buf_obs))
+
+    def __init__(
+        self,
+        env: gym.Env,
+    ):
+        self.env = _patch_env(env)
+        env = self.env
+        super().__init__(1, env.observation_space, env.action_space)
+        obs_space = env.observation_space
+        self.keys, shapes, dtypes = obs_space_info(obs_space)
+
+        self.buf_obs = OrderedDict(
+            [
+                (k, np.zeros((self.num_envs, *tuple(shapes[k])), dtype=dtypes[k]))
+                for k in self.keys
+            ]
         )
-        return (
-            {
-                "vision": rgb,
-                "sound": np.array(sound_vector, dtype=np.float32),
-            },
-            reward,
-            terminated,
-            truncated,
-            info,
-        )  # , done: deprecated
+        self.buf_dones = np.zeros((self.num_envs,), dtype=bool)
+        self.buf_rews = np.zeros((self.num_envs,), dtype=np.float32)
+        self.buf_infos: List[Dict[str, Any]] = [{} for _ in range(self.num_envs)]
+        self.metadata = env.metadata
 
     def reset(
         self,
         *,
         seed: Optional[int] = None,
     ):
-        obs, info = self.env.reset(seed=seed, options={"fast_reset": True})
-        rgb = info["rgb"]
-        obs_info = info["obs"]
-        sound_subtitles = obs_info.sound_subtitles
-        sound_vector = self.encode_sound(
-            sound_subtitles, obs_info.x, obs_info.y, obs_info.z, obs_info.yaw
-        )
-        return {
-            "vision": rgb,
-            "sound": np.array(sound_vector, dtype=np.float32),
-        }, info
-
-    def encode_sound(
-        self, sound_subtitles: List, x: float, y: float, z: float, yaw: float
-    ) -> List[float]:
-        sound_vector = [0] * (len(self.sound_list) * self.sound_coord_dim + 3)
-        for sound in sound_subtitles:
-            if sound.x - x > 16 or sound.z - z > 16:
-                continue
-            if sound.x - x < -16 or sound.z - z < -16:
-                continue
-            if self.sound_coord_dim == 3 and (sound.y - y < -16 or sound.y - y > 16):
-                continue
-            for idx, translation_key in enumerate(self.sound_list):
-                if translation_key == sound.translate_key:
-                    dx = sound.x - x
-                    if self.sound_coord_dim == 3:
-                        dy = sound.y - y
-                    else:
-                        dy = 0
-                    dz = sound.z - z
-                    if self.sound_coord_dim == 2:
-                        sound_vector[idx * self.sound_coord_dim] = dx / 15
-                        sound_vector[idx * self.sound_coord_dim + 1] = dz / 15
-                    else:
-                        sound_vector[idx * self.sound_coord_dim] = dx / 15
-                        sound_vector[idx * self.sound_coord_dim + 1] = dy / 15
-                        sound_vector[idx * self.sound_coord_dim + 2] = dz / 15
-                elif translation_key == "subtitles.entity.player.hurt":
-                    sound_vector[-1] = 1  # player hurt sound
-
-        # Trigonometric encoding
-        yaw_radians = math.radians(yaw)
-        sound_vector[-3] = math.cos(yaw_radians)
-        sound_vector[-2] = math.sin(yaw_radians)
-
-        return sound_vector
+        obs, self.reset_infos[0] = self.env.reset(seed=self._seeds[0])
+        self._save_obs(0, obs)
+        # Seeds are only used once
+        self._reset_seeds()
+        return self._obs_from_buf()
