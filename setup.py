e2693e40c95533eb41adddbe6db867ddd006320f
yhs0602 <jourhyang123@gmail.com>
2024-02-02 16:38:05 +0900

    :bug: Bug

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 packages = find_packages()
 setup(
     name="craftground",
-    version="1.3.6",
+    version="1.3.7",
     packages=packages,
     install_requires=["gymnasium", "Pillow", "numpy", "protobuf", "typing_extensions"],
     author="yhs0602",
