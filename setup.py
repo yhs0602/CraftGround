a6807e2d1b6c615a850d02dffde281c32b7d0e3f
yhs0602 <jourhyang123@gmail.com>
2024-04-05 20:58:06 +0900

    :bug: Fix pnglib

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 packages = find_packages()
 setup(
     name="craftground",
-    version="1.6.3",
+    version="1.6.4",
     packages=packages,
     install_requires=["gymnasium", "Pillow", "numpy", "protobuf", "typing_extensions"],
     author="yhs0602",
