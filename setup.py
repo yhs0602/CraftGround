385eb7bb06589ea054a1d7c5bae369ee5b88b257
yhs0602 <jourhyang123@gmail.com>
2024-04-05 21:12:50 +0900

    :bug: Fix png

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 packages = find_packages()
 setup(
     name="craftground",
-    version="1.6.4",
+    version="1.6.5",
     packages=packages,
     install_requires=["gymnasium", "Pillow", "numpy", "protobuf", "typing_extensions"],
     author="yhs0602",
