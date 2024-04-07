de52628be0b6e35264c154e56c9a7bef65671a0a
yhs0602 <jourhyang123@gmail.com>
2024-04-08 00:28:11 +0900

    :zap: Use lithium

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 packages = find_packages()
 setup(
     name="craftground",
-    version="1.7.8",
+    version="1.7.9",
     packages=packages,
     install_requires=["gymnasium", "Pillow", "numpy", "protobuf", "typing_extensions"],
     author="yhs0602",
