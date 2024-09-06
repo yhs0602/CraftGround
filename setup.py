1a2450f07f7d3371cb3f735baec515a6bca09d50
yhs0602 <jourhyang123@gmail.com>
2024-09-06 19:27:25 +0900

    :bug: Set xy of mouse

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 packages = find_packages()
 setup(
     name="craftground",
-    version="2.4.17",
+    version="2.4.18",
     packages=packages,
     install_requires=["gymnasium", "Pillow", "numpy", "protobuf", "typing_extensions"],
     author="yhs0602",
