a63f427c86b6e9eee2f1c6c071aea394d529867d
yhs0602 <jourhyang123@gmail.com>
2024-09-22 17:06:24 +0900

    :fire: Disbale biome cache

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 packages = find_packages()
 setup(
     name="craftground",
-    version="2.4.71",
+    version="2.4.72",
     packages=packages,
     install_requires=["gymnasium", "Pillow", "numpy", "protobuf", "typing_extensions"],
     author="yhs0602",
