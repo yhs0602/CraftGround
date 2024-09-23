51f469a116a4c5844a5c09e46e05792b8aaad0fc
yhs0602 <jourhyang123@gmail.com>
2024-09-23 21:55:09 +0900

    :necktie: Change observation space viso

--- craftground/wrappers/vision.py
+++ craftground/wrappers/vision.py
@@ -13,7 +13,7 @@ class VisionWrapper(gym.Wrapper):
         self.observation_space = gym.spaces.Box(
             low=0,
             high=255,
-            shape=(3, x_dim, y_dim),
+            shape=(x_dim, y_dim, 3),
             dtype=np.uint8,
         )
 
