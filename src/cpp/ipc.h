0053d6e66dc123993d9f7532d77636145cd54dd6
Hyeonseo Yang <yhs0602@snu.ac.kr>
2024-12-29 11:19:11 +0900

    🐛 Fix header

--- src/cpp/ipc.h
+++ src/cpp/ipc.h
@@ -3,7 +3,7 @@
 #include "../../pybind11/include/pybind11/pybind11.h"
 
 namespace py = pybind11;
-py::object initialize_from_mach_port(int machPort, int width, int height);
+py::object initialize_from_mach_port(unsigned int machPort, int width, int height);
 py::object
 mtl_tensor_from_cuda_mem_handle(void *cuda_ipc_handle, int width, int height);
 
