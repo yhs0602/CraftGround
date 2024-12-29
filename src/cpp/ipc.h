a86be1b0e759117df03fc00fa3324dcb0c3ccac2
Hyeonseo Yang <yhs0602@snu.ac.kr>
2024-12-29 13:08:14 +0900

    🐛 Fix bytes type

--- src/cpp/ipc.h
+++ src/cpp/ipc.h
@@ -2,10 +2,11 @@
 #define __IPC_H__
 #include "../../pybind11/include/pybind11/pybind11.h"
 #include "../../pybind11/include/pybind11/stl.h"
+#include "../../pybind11/include/pybind11/pytypes.h"
 
 namespace py = pybind11;
 py::object initialize_from_mach_port(unsigned int machPort, int width, int height);
 py::object
-mtl_tensor_from_cuda_mem_handle(const std::vector<uint8_t>& cuda_ipc_handle, int width, int height);
+mtl_tensor_from_cuda_mem_handle(const char* cuda_ipc_handle, int width, int height);
 
 #endif
