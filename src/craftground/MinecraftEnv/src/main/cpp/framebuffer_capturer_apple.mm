0b14f20fa593a24b656cecc7c0863fd1d03d0d89
Hyeonseo Yang <yhs0602@snu.ac.kr>
2024-12-31 21:14:57 +0900

    🐛 Fix color

--- src/craftground/MinecraftEnv/src/main/cpp/framebuffer_capturer_apple.mm
+++ src/craftground/MinecraftEnv/src/main/cpp/framebuffer_capturer_apple.mm
@@ -108,7 +108,7 @@ int initializeIoSurface(
         GL_RGBA,
         width,
         height,
-        GL_BGRA,
+        GL_RGBA,
         GL_UNSIGNED_INT_8_8_8_8_REV,
         ioSurface,
         0
