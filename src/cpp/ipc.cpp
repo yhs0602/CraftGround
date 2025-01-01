55264f1033641a157014186d150e3f9e6e5755b7
Hyeonseo Yang <yhs0602@snu.ac.kr>
2025-01-01 12:26:41 +0900

    :bug: Fix build error

--- src/cpp/ipc.cpp
+++ src/cpp/ipc.cpp
@@ -9,7 +9,7 @@ py::object
 initialize_from_mach_port(unsigned int machPort, int width, int height) {
     return mtl_tensor_from_mach_port(machPort, width, height);
 }
-py::object mtl_tensor_from_cuda_mem_handle(
+py::capsule mtl_tensor_from_cuda_mem_handle(
     const char *cuda_ipc_handle, int width, int height
 ) {
     return py::none();
@@ -17,7 +17,7 @@ py::object mtl_tensor_from_cuda_mem_handle(
 
 #elif HAS_CUDA
 #include "ipc_cuda.h"
-py::object initialize_from_mach_port(int machPort, int width, int height) {
+py::object initialize_from_mach_port(unsigned int machPort, int width, int height) {
     return py::none();
 }
 
@@ -25,7 +25,7 @@ py::capsule mtl_tensor_from_cuda_mem_handle(
     const char *cuda_ipc_handle, int width, int height
 ) {
     DLManagedTensor *tensor = mtl_tensor_from_cuda_ipc_handle(
-        const_cast<void *>(cuda_ipc_handle), width, height
+        reinterpret_cast<void*>(const_cast<char*>(cuda_ipc_handle)), width, height
     );
 
     if(!tensor) {
@@ -50,7 +50,7 @@ initialize_from_mach_port(unsigned int machPort, int width, int height) {
     return py::none();
 }
 
-py::object mtl_tensor_from_cuda_mem_handle(
+py::capsule mtl_tensor_from_cuda_mem_handle(
     const char *cuda_ipc_handle, int width, int height
 ) {
     return py::none();
