803ab6e382a909b124bbb7052f70bdc37f358cc5
Hyeonseo Yang <yhs0602@snu.ac.kr>
2025-01-08 19:01:31 +0900

    ✅ Pass test

--- tests/cpp/test_ipc.cpp
+++ tests/cpp/test_ipc.cpp
@@ -1,12 +1,14 @@
 #include <gtest/gtest.h>
 #include <pybind11/pybind11.h>
+#include <stdexcept>
 #include "ipc.h"
 
 TEST(IPCModuleTest, AddFunction) {
-    EXPECT_EQ(initialize_from_mach_port(2, 3, 4), py::none());
+    EXPECT_THROW(initialize_from_mach_port(2, 3, 4), std::runtime_error);
 }
 
 // TEST(IPCModuleTest, ExceptionHandling) {
+// EXPECT_EQ
 //     EXPECT_THROW(
 //         mtl_tensor_from_cuda_mem_handle("", 0, 2), std::invalid_argument
 //     );
