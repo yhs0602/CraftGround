ffbaa566cecd721d1f6dae79d42ff7f20094afcd
Hyeonseo Yang <yhs0602@snu.ac.kr>
2025-02-09 01:21:55 +0900

    ✨ Somewhat snbt

--- src/craftground/nbt/nbt_dataclass.py
+++ src/craftground/nbt/nbt_dataclass.py
@@ -20,8 +20,8 @@ T = TypeVar(
         "NBTString",
         "NBTList[Any]",
         "NBTCompound[Any]",
-        "NBTIntArrayType",
-        "NBTLongArrayType",
+        "NBTIntArray",
+        "NBTLongArray",
     ],
 )
 
@@ -34,6 +34,9 @@ class NBTBase:
     def __repr__(self):
         return f"{self.__class__.__name__}({self.value})"
 
+    def to_snbt(self, indent: int, depth: int) -> str:
+        raise NotImplementedError
+
 
 class NBTByte(NBTBase):
     def __init__(self, value: bool):
@@ -41,6 +44,9 @@ class NBTByte(NBTBase):
 
     def to_nbt_contents(self) -> NbtContents:
         return NbtContents(TagType.ByteType, self.value)
+
+    def to_snbt(self, indent=0, depth=0):
+        return f"{self.value}b"
 
 
 class NBTShort(NBTBase):
@@ -50,6 +56,9 @@ class NBTShort(NBTBase):
     def to_nbt_contents(self) -> NbtContents:
         return NbtContents(TagType.ShortType, self.value)
 
+    def to_snbt(self, indent=0, depth=0):
+        return f"{self.value}s"
+
 
 class NBTInt(NBTBase):
     def __init__(self, value: int):
@@ -57,6 +66,9 @@ class NBTInt(NBTBase):
 
     def to_nbt_contents(self) -> NbtContents:
         return NbtContents(TagType.IntType, self.value)
+
+    def to_snbt(self, indent=0, depth=0):
+        return f"{self.value}"
 
 
 class NBTLong(NBTBase):
@@ -66,6 +78,9 @@ class NBTLong(NBTBase):
     def to_nbt_contents(self) -> NbtContents:
         return NbtContents(TagType.LongType, self.value)
 
+    def to_snbt(self, indent=0, depth=0):
+        return f"{self.value}l"
+
 
 class NBTFloat(NBTBase):
     def __init__(self, value: float):
@@ -73,6 +88,9 @@ class NBTFloat(NBTBase):
 
     def to_nbt_contents(self) -> NbtContents:
         return NbtContents(TagType.FloatType, self.value)
+
+    def to_snbt(self, indent=0, depth=0):
+        return f"{self.value}f"
 
 
 class NBTDouble(NBTBase):
@@ -82,6 +100,9 @@ class NBTDouble(NBTBase):
     def to_nbt_contents(self) -> NbtContents:
         return NbtContents(TagType.DoubleType, self.value)
 
+    def to_snbt(self, indent=0, depth=0):
+        return f"{self.value}d"
+
 
 class NBTByteArray(NBTBase):
     def __init__(self, value: List[int]):
@@ -89,6 +110,9 @@ class NBTByteArray(NBTBase):
 
     def to_nbt_contents(self) -> NbtContents:
         return NbtContents(TagType.ByteArrayType, self.value)
+
+    def to_snbt(self, indent=0, depth=0):
+        return f"[B;{','.join(f'{v}b' for v in self.value)}]"
 
 
 class NBTString(NBTBase):
@@ -98,21 +122,38 @@ class NBTString(NBTBase):
     def to_nbt_contents(self) -> NbtContents:
         return NbtContents(TagType.StringType, self.value)
 
+    def to_snbt(self, indent=0, depth=0):
+        """
+                A string enclosed in quotes. For strings containing only 0-9, A-Z, a-z, _, -, ., and +, and not confused with other data types, quote enclosure is optional. Quotes can be either single quote ' or double ". Nested quotes can be included within a string by escaping the character with a \ escape. \s that are supposed to show up as \ need to be escaped to \\.
+        <[a-zA-Z0-9_\-\.\+] text>, "<text>" (" within needs to be escaped to \"), or '<text>' (' within needs to be escaped to \')
+        """
+        # Escape quotes and backslashes
+        escaped = (
+            self.value.replace("\\", "\\\\").replace('"', '\\"').replace("'", "\\'")
+        )
+        return f'"{escaped}"'
 
-class NBTIntArrayType(NBTBase):
+
+class NBTIntArray(NBTBase):
     def __init__(self, value: List[int]):
         self.value = value
 
     def to_nbt_contents(self) -> NbtContents:
         return NbtContents(TagType.IntArrayType, self.value)
 
+    def to_snbt(self, indent=0, depth=0):
+        return f"[I;{','.join(str(v) for v in self.value)}]"
 
-class NBTLongArrayType(NBTBase):
+
+class NBTLongArray(NBTBase):
     def __init__(self, value: List[int]):
         self.value = value
 
     def to_nbt_contents(self) -> NbtContents:
         return NbtContents(TagType.LongArrayType, self.value)
+
+    def to_snbt(self, indent=0, depth=0):
+        return f"[L;{','.join(str(v) for v in self.value)}]"
 
 
 # Recursive NBTList and NBTCompound
@@ -123,16 +164,43 @@ class NBTList(NBTBase, Generic[T]):
     def to_nbt_contents(self) -> NbtContents:
         return NbtContents(TagType.ListType, [v.to_nbt_contents() for v in self.value])
 
+    def to_snbt(self, indent=0, depth=0):
+        if indent:
+            items = [v.to_snbt(indent=indent, depth=depth + 1) for v in self.value]
+            inner_indent = " " * (indent * (depth + 1))
+            return (
+                "[\n"
+                + ",\n".join(inner_indent + v for v in items)
+                + f"\n{' ' * (indent * depth)}]"
+            )
+        items = [v.to_snbt() for v in self.value]
+        return "[" + ",".join(items) + "]"
+
 
 class NBTCompound(NBTBase, Dict[str, NBTBase]):
     def __init__(self, value: Dict[str, NBTBase]):
         self.value = value
 
-    def to_nbt_contents(self) -> NbtContents:
+    def to_nbt_contents(self, indent=0, depth=0) -> NbtContents:
         return NbtContents(
             TagType.CompoundType,
             {k: v.to_nbt_contents() for k, v in self.value.items()},
         )
+
+    def to_snbt(self, indent=0, depth=0):
+        if indent:
+            items = [
+                f"{k}:{v.to_snbt(indent=indent, depth=depth + 1)}"
+                for k, v in self.items()
+            ]
+            inner_indent = " " * (indent * (depth + 1))
+            return (
+                "{\n"
+                + ",\n".join(inner_indent + item for item in items)
+                + f"\n{' ' * (indent * depth)}}}"
+            )
+        items = [f"{k}:{v.to_snbt()}" for k, v in self.value.items()]
+        return "{" + ",".join(items) + "}"
 
 
 U = TypeVar("U", bound="NBTSerializable")
@@ -236,12 +304,34 @@ class NBTSerializable:
         elif value.tag_type == TagType.ByteArrayType:
             return NBTByteArray(value.value)
         elif value.tag_type == TagType.IntArrayType:
-            return NBTIntArrayType(value.value)
+            return NBTIntArray(value.value)
         elif value.tag_type == TagType.LongArrayType:
-            return NBTLongArrayType(value.value)
+            return NBTLongArray(value.value)
         elif value.tag_type == TagType.StringType:
             return NBTString(value.value)
         elif value.tag_type == TagType.CompoundType:
             return NBTCompound(value.value)
         else:
             return value  # Return raw if no explicit mapping exists
+
+    def to_snbt(self, indent: int = 0, depth_outer: int = 0) -> str:
+        # Convert dataclass fields into SNBT format
+        snbt_fields = []
+        for field in fields(self):
+            value = object.__getattribute__(self, field.name)
+            if value is not None:
+                if indent:
+                    snbt_fields.append(
+                        f"{field.name}:{value.to_snbt(indent, depth_outer + 1)}"
+                    )
+                else:
+                    snbt_fields.append(f"{field.name}:{value.to_snbt()}")
+
+        if indent:
+            inner_indent = " " * (indent * (depth_outer + 1))
+            return (
+                "{\n"
+                + ",\n".join(inner_indent + field for field in snbt_fields)
+                + f"\n{' ' * (indent * depth_outer)}}}"
+            )
+        return "{" + ",".join(snbt_fields) + "}"
