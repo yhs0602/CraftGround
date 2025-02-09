b538e33928a2f85bd75f686598bfe571e0afa366
Hyeonseo Yang <yhs0602@snu.ac.kr>
2025-02-09 17:04:50 +0900

    ✨ Add more structs

--- src/craftground/nbt/xml_to_nbt.py
+++ src/craftground/nbt/xml_to_nbt.py
@@ -10,37 +10,34 @@ def parse_drawing_decorator(xml_file: str, output_nbt: str):
 
     structure = Structure()
 
-    # Find the DrawingDecorator section
-    drawing_decorator = root.find(".//malmo:DrawingDecorator", namespace)
-    if drawing_decorator is None:
-        raise ValueError("No DrawingDecorator found in XML.")
+    # Find all DrawingDecorator sections
+    for drawing_decorator in root.findall(".//malmo:DrawingDecorator", namespace):
+        # Process elements in order to preserve drawing priority
+        for element in drawing_decorator:
+            tag = element.tag.split("}")[-1]  # Remove namespace prefix
 
-    # Process elements in order to preserve drawing priority
-    for element in drawing_decorator:
-        tag = element.tag.split("}")[-1]  # Remove namespace prefix
+            if tag == "DrawCuboid":
+                block_type = element.get("type")
+                x1, y1, z1 = (
+                    int(element.get("x1")),
+                    int(element.get("y1")),
+                    int(element.get("z1")),
+                )
+                x2, y2, z2 = (
+                    int(element.get("x2")),
+                    int(element.get("y2")),
+                    int(element.get("z2")),
+                )
+                structure.set_cuboid(x1, y1, z1, x2, y2, z2, f"minecraft:{block_type}")
 
-        if tag == "DrawCuboid":
-            block_type = element.get("type")
-            x1, y1, z1 = (
-                int(element.get("x1")),
-                int(element.get("y1")),
-                int(element.get("z1")),
-            )
-            x2, y2, z2 = (
-                int(element.get("x2")),
-                int(element.get("y2")),
-                int(element.get("z2")),
-            )
-            structure.set_cuboid(x1, y1, z1, x2, y2, z2, f"minecraft:{block_type}")
-
-        elif tag == "DrawBlock":
-            block_type = element.get("type")
-            x, y, z = (
-                int(element.get("x")),
-                int(element.get("y")),
-                int(element.get("z")),
-            )
-            structure.set_block(x, y, z, f"minecraft:{block_type}")
+            elif tag == "DrawBlock":
+                block_type = element.get("type")
+                x, y, z = (
+                    int(element.get("x")),
+                    int(element.get("y")),
+                    int(element.get("z")),
+                )
+                structure.set_block(x, y, z, f"minecraft:{block_type}")
 
     # Save the parsed structure as an NBT file
     structure.save(output_nbt)
@@ -49,4 +46,4 @@ def parse_drawing_decorator(xml_file: str, output_nbt: str):
 
 # Example usage
 if __name__ == "__main__":
-    parse_drawing_decorator("nbts/mission.xml", "nbts/mission.nbt")
+    parse_drawing_decorator("nbts/tricky_arena.xml", "nbts/tricky_arena.nbt")
